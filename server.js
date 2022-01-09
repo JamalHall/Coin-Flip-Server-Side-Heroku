377d1739c379e41715f7ec7030310ea46bd2f682
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-08 21:46:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 8000
 
-app.use(cors())
+//app.use(cors())
 
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`)

290f61aa54c971cda6a641a2bcf9bfcb320d9d7b
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-09 19:08:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -42,11 +42,11 @@ let coin =  {
 
     'heads':{
         'msg': 'heads',
-        'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQaeG3xye5CKqHNy2WYOX3fYgc5xUtug96INkoRafAblcqtDDhO_bCSi_dJ-hsesOGCE8&usqp=CAU'  , 
+        'imageUrl': 'https://media.cnn.com/api/v1/images/stellar/prod/170113070623-lady-liberty-coin-front.jpg?q=x_0,y_0,h_1125,w_1999,c_fill/h_540,w_960'  , 
     },
 
     'tails':{
         'msg': 'tails',
-        'imageUrl': 'https://catalog.usmint.gov/dw/image/v2/AARB_PRD/on/demandware.static/-/Sites-usm-master-catalog-us/default/dw36d1fa3b/images/hi-res/coins/Gold-Coins/17xa_b.jpg?sw=50'  ,
+        'imageUrl': 'https://media.cnn.com/api/v1/images/stellar/prod/170113070626-lady-liberty-coin-back.jpg?q=x_2,y_0,h_1123,w_1995,c_crop/h_540,w_960'  ,
     },
 }
